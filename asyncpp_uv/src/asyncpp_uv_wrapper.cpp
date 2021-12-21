1ceec054ba4040522b8933b5584bf4338b62b2e2
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-12-21 03:02:40 +0100

    asyncpp_uv: uvTimerStart returns TimerHandle with cancellation support

--- asyncpp_uv/src/asyncpp_uv_wrapper.cpp
+++ asyncpp_uv/src/asyncpp_uv_wrapper.cpp
@@ -8,22 +8,45 @@ void uvCloseDelete(void* handle) {
   uv_close((uv_handle_t*)handle, [](uv_handle_t* handleInner) { delete handleInner; });
 }
 
-void uvTimerStart(uint64_t timeoutMs, const TimerCallback& cb, uv_loop_t* loop) {
+using TimerDataHandleSPtr = std::shared_ptr<TimerHandle>;
+
+void TimerHandle::cancel() {
+  if (this->handle == nullptr) return;
+
+  uv_timer_t* uvHandle = (uv_timer_t*)this->handle;
+
+  TimerDataHandleSPtr* timerHandleInner = (TimerDataHandleSPtr*)uvHandle->data;
+  delete timerHandleInner;
+
+  uv_timer_stop(uvHandle);
+  uvCloseDelete(uvHandle);
+
+  this->handle = nullptr;
+}
+
+std::shared_ptr<TimerHandle> uvTimerStart(uint64_t timeoutMs, const TimerCallback& cb, uv_loop_t* loop) {
   if (loop == nullptr) loop = uv_default_loop();
 
-  uv_timer_t* handle = new uv_timer_t();
-  handle->data = new TimerCallback(cb);
+  uv_timer_t* uvHandle = new uv_timer_t();
 
-  uv_timer_init(loop, handle);
+  std::shared_ptr<TimerHandle> timerHandle = std::make_shared<TimerHandle>(uvHandle, cb);
+
+  uvHandle->data = new TimerDataHandleSPtr(timerHandle);
+
+  uv_timer_init(loop, uvHandle);
   uv_timer_start(
-      handle,
-      [](uv_timer_t* handleInner) {
-        TimerCallback* data = (TimerCallback*)handleInner->data;
-        (*data)();
-        uv_timer_stop(handleInner);
-        delete data;
-        uvCloseDelete(handleInner);
+      uvHandle,
+      [](uv_timer_t* uvHandleInner) {
+        TimerDataHandleSPtr* timerHandleInner = (TimerDataHandleSPtr*)uvHandleInner->data;
+        (*timerHandleInner)->callback();
+        (*timerHandleInner)->handle = nullptr;
+        delete timerHandleInner;
+
+        uv_timer_stop(uvHandleInner);
+        uvCloseDelete(uvHandleInner);
       },
       timeoutMs, 0);
+
+  return timerHandle;
 }
 }  // namespace asyncpp_uv::wrapper

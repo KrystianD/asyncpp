a4f508f288190c3df19f799299ae87982c800fec
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-12-21 02:45:46 +0100

    asyncpp_uv: extracted TimerCallback using

--- asyncpp_uv/src/asyncpp_uv_wrapper.cpp
+++ asyncpp_uv/src/asyncpp_uv_wrapper.cpp
@@ -8,15 +8,15 @@ void uvCloseDelete(void* handle) {
   uv_close((uv_handle_t*)handle, [](uv_handle_t* handleInner) { delete handleInner; });
 }
 
-void uvTimerStart(uv_loop_t* loop, uint64_t timeoutMs, const std::function<void()>& cb) {
+void uvTimerStart(uv_loop_t* loop, uint64_t timeoutMs, const TimerCallback& cb) {
   uv_timer_t* handle = new uv_timer_t();
-  handle->data = new std::function<void()>(cb);
+  handle->data = new TimerCallback(cb);
 
   uv_timer_init(loop, handle);
   uv_timer_start(
       handle,
       [](uv_timer_t* handleInner) {
-        std::function<void()>* data = (std::function<void()>*)handleInner->data;
+        TimerCallback* data = (TimerCallback*)handleInner->data;
         (*data)();
         uv_timer_stop(handleInner);
         delete data;

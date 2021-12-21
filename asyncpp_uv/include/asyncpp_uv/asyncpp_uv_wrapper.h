deef1df86c876e20aef79f9b8bf2785a611ffcfe
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-12-21 03:02:40 +0100

    asyncpp_uv: uvTimerStart returns TimerHandle with cancellation support

--- asyncpp_uv/include/asyncpp_uv/asyncpp_uv_wrapper.h
+++ asyncpp_uv/include/asyncpp_uv/asyncpp_uv_wrapper.h
@@ -4,8 +4,27 @@
 
 #include <cstdint>
 #include <functional>
+#include <memory>
 
 namespace asyncpp_uv::wrapper {
 using TimerCallback = std::function<void()>;
-void uvTimerStart(uint64_t timeoutMs, const TimerCallback& cb, uv_loop_t* loop = nullptr);
+
+class TimerHandle {
+  void* handle;
+  TimerCallback callback;
+
+  TimerHandle(const TimerHandle&) = delete;
+  TimerHandle& operator=(const TimerHandle&) = delete;
+  TimerHandle(TimerHandle&&) = delete;
+  TimerHandle& operator=(TimerHandle&&) = delete;
+
+ public:
+  TimerHandle(void* handle, TimerCallback callback) : handle(handle), callback(callback) {}
+
+  void cancel();
+
+  friend std::shared_ptr<TimerHandle> uvTimerStart(uint64_t timeoutMs, const TimerCallback& cb, uv_loop_t* loop);
+};
+
+std::shared_ptr<TimerHandle> uvTimerStart(uint64_t timeoutMs, const TimerCallback& cb, uv_loop_t* loop = nullptr);
 }  // namespace asyncpp_uv::wrapper

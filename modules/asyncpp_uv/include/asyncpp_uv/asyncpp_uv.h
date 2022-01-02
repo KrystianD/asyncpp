8a5b0bb5f1bb9727c0f79fa3e443d9b1c7a0fa33
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-01-02 21:39:56 +0100

    asyncpp_uv: added wrappers for close/stream/tcp

--- modules/asyncpp_uv/include/asyncpp_uv/asyncpp_uv.h
+++ modules/asyncpp_uv/include/asyncpp_uv/asyncpp_uv.h
@@ -5,6 +5,13 @@
 #include <asyncpp/asyncpp.h>
 
 namespace asyncpp_uv {
+asyncpp::task<void> uvCloseAsync(uv_handle_t* handle);
+
 asyncpp::task<void> uvSleepAsync(uint64_t timeoutMs);
 asyncpp::task<void> uvSleepAsync(const std::chrono::milliseconds& duration);
+
+asyncpp::task<int> uvShutdownAsync(uv_stream_t* stream);
+asyncpp::task<int> uvWriteAsync(uv_stream_t* stream, const void* data, size_t length);
+
+asyncpp::task<int> uvTcpConnectAsync(uv_tcp_t* tcp, std::string host, uint16_t port);
 }  // namespace asyncpp_uv
--- modules/asyncpp_uv/src/asyncpp_uv.cpp
+++ modules/asyncpp_uv/src/asyncpp_uv.cpp
@@ -2,6 +2,10 @@
 #include <uvpp/uvpp.h>
 
 namespace asyncpp_uv {
+asyncpp::task<void> uvCloseAsync(uv_handle_t* handle) {
+  return asyncpp::makeTask([handle](auto resolve) { uvpp::uvClose(handle, [resolve](uv_handle_t*) { resolve(); }); });
+}
+
 asyncpp::task<void> uvSleepAsync(uint64_t timeoutMs) {
   return asyncpp::makeTask([timeoutMs](auto resolve) { uvpp::uvTimerStart(timeoutMs, resolve); });
 }
@@ -9,4 +13,17 @@ asyncpp::task<void> uvSleepAsync(uint64_t timeoutMs) {
 asyncpp::task<void> uvSleepAsync(const std::chrono::milliseconds& duration) {
   return uvSleepAsync((uint64_t)duration.count());
 }
+
+asyncpp::task<int> uvShutdownAsync(uv_stream_t* stream) {
+  return asyncpp::makeTask<int>([stream](auto resolve) { uvpp::uvShutdown(stream, resolve); });
+}
+
+asyncpp::task<int> uvWriteAsync(uv_stream_t* stream, const void* data, size_t length) {
+  return asyncpp::makeTask<int>([stream, data, length](auto resolve) { uvpp::uvWrite(stream, data, length, resolve); });
+}
+
+asyncpp::task<int> uvTcpConnectAsync(uv_tcp_t* tcp, std::string host, uint16_t port) {
+  return asyncpp::makeTask<int>([tcp, host, port](auto resolve) { uvpp::uvTcpConnect(tcp, host, port, resolve); });
+}
+
 }  // namespace asyncpp_uv

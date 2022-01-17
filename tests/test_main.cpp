5c69a907d227d00556c40eb2bf4dd2c5e9d34114
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-01-17 17:57:51 +0100

    tests: keep running uv loop until all handles are completed

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,11 +3,11 @@
 int main(int argc, char *argv[]) {
   bool stop = false;
   std::thread th([&stop]() {
-    while (!stop) {
-      int res;
+    int res;
+    do {
       res = uv_run(uv_default_loop(), UV_RUN_ONCE);
       if (res == 0) uv_sleep(100);
-    }
+    } while (!stop || res != 0);
   });
   ::testing::InitGoogleTest(&argc, argv);
   int result = RUN_ALL_TESTS();

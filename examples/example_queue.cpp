54b32df4dcbdeae0e99ba6cd21e7d00917b57b7e
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-01-18 19:39:10 +0100

    added uvRunInThread and uvRunInThreadAsync examples

---
+++ examples/example_queue.cpp
@@ -0,0 +1,39 @@
+#include <uv.h>
+
+#include <asyncpp_uv/asyncpp_uv.h>
+#include <uvpp/uvpp.h>
+
+using namespace std;
+using namespace asyncpp;
+using namespace asyncpp_uv;
+
+long expensiveWork() {
+  long v = 0;
+  for (long i = 0; i < 1000UL * 1000UL * 1000UL * 10UL; i++) {
+    v += i;
+  }
+  return v;
+}
+
+int main() {
+  //  uvpp::uvRunInThread<long>(expensiveWork, [](long res) { printf("res1 %ld\n", res); });
+  //  uvpp::uvRunInThread<long>(expensiveWork, [](long res) { printf("res2 %ld\n", res); });
+  //  uvpp::uvRunInThread<long>(expensiveWork, [](long res) { printf("res3 %ld\n", res); });
+
+  asyncpp::run([]() -> task<void> {
+    task<long> res1_task = asyncpp_uv::uvRunInThreadAsync<long>([]() { return expensiveWork(); });
+    task<long> res2_task = asyncpp_uv::uvRunInThreadAsync<long>([]() { return expensiveWork(); });
+    task<long> res3_task = asyncpp_uv::uvRunInThreadAsync<long>([]() { return expensiveWork(); });
+
+    long res1 = co_await res1_task;
+    long res2 = co_await res2_task;
+    long res3 = co_await res3_task;
+
+    printf("res1 %lu\n", res1);
+    printf("res2 %lu\n", res2);
+    printf("res3 %lu\n", res3);
+  });
+
+  uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+  return 0;
+}

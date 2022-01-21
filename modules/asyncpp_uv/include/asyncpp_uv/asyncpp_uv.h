300e475b1e6365759ce8f178197de7c5cfbfea79
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-01-21 23:26:27 +0100

    asyncpp_uv: added uvRunInThreadAsync

--- modules/asyncpp_uv/include/asyncpp_uv/asyncpp_uv.h
+++ modules/asyncpp_uv/include/asyncpp_uv/asyncpp_uv.h
@@ -5,6 +5,7 @@
 #include <chrono>
 
 #include <asyncpp/asyncpp.h>
+#include <uvpp/uvpp.h>
 
 namespace asyncpp_uv {
 asyncpp::task<void> uvCloseAsync(uv_handle_t* handle);
@@ -16,4 +17,10 @@ asyncpp::task<int> uvShutdownAsync(uv_stream_t* stream);
 asyncpp::task<int> uvWriteAsync(uv_stream_t* stream, const void* data, size_t length);
 
 asyncpp::task<int> uvTcpConnectAsync(uv_tcp_t* tcp, std::string host, uint16_t port);
+
+template<typename T>
+asyncpp::task<T> uvRunInThreadAsync(uvpp::WorkCallback<T> workCb) {
+  return asyncpp::makeTask<T>(
+      [workCb](auto resolve) { uvpp::uvRunInThread<T>(workCb, [resolve](T value) { resolve(std::move(value)); }); });
+}
 }  // namespace asyncpp_uv

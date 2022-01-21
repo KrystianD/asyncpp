d1251cb42846bf6471d4c8dd239b4b8b403271b3
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-01-21 20:34:41 +0100

    added uvAsyncSend example

---
+++ examples/example_async.cpp
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <uv.h>
+
+#include <thread>
+
+#include <asyncpp/asyncpp.h>
+#include <asyncpp_uv/asyncpp_uv.h>
+#include <uvpp/uvpp.h>
+
+using namespace std;
+using namespace asyncpp;
+using namespace asyncpp_uv;
+
+int getThreadId() { return gettid(); }
+
+void callback() { printf("callback (thread: %d)\n", getThreadId()); }
+
+int main() {
+  printf("main (thread: %d)\n", getThreadId());
+
+  std::thread th([]() {
+    for (int i = 0; i < 3; i++) {
+      usleep(100 * 1000);
+
+      printf("loop (thread: %d)\n", getThreadId());
+
+      uvpp::uvAsyncSend([]() { callback(); });
+    }
+  });
+
+  asyncpp::run([]() -> task<void> { co_await uvSleepAsync(1000ms); });
+
+  uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+
+  th.join();
+
+  return 0;
+}
